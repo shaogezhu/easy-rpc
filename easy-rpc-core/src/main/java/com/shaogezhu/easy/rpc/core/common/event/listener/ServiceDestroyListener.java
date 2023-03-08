4fbdb7d183707bd80f5cf74ec1e74e9d73abdd4f
shaogezhu <1090404459@qq.com>
2023-03-09 00:05:05 +0800

    fix: 修复服务端下线报错

--- easy-rpc-core/src/main/java/com/shaogezhu/easy/rpc/core/common/event/listener/ServiceDestroyListener.java
+++ easy-rpc-core/src/main/java/com/shaogezhu/easy/rpc/core/common/event/listener/ServiceDestroyListener.java
@@ -3,6 +3,8 @@ package com.shaogezhu.easy.rpc.core.common.event.listener;
 import com.shaogezhu.easy.rpc.core.common.event.RpcDestroyEvent;
 import com.shaogezhu.easy.rpc.core.registy.URL;
 
+import java.util.Iterator;
+
 import static com.shaogezhu.easy.rpc.core.common.cache.CommonServerCache.PROVIDER_URL_SET;
 import static com.shaogezhu.easy.rpc.core.common.cache.CommonServerCache.REGISTRY_SERVICE;
 
@@ -15,7 +17,10 @@ public class ServiceDestroyListener implements RpcListener<RpcDestroyEvent> {
 
     @Override
     public void callBack(Object t) {
-        for (URL url : PROVIDER_URL_SET) {
+        Iterator<URL> urlIterator = PROVIDER_URL_SET.iterator();
+        while (urlIterator.hasNext()) {
+            URL url = urlIterator.next();
+            urlIterator.remove();
             REGISTRY_SERVICE.unRegister(url);
         }
     }
